28d302d516fb189b5f642a520e248552d0840086
Server048 <64312413+Server048@users.noreply.github.com>
2021-10-06 10:52:51 +0800

    Create bagus.sh

---
+++ bagus.sh
@@ -0,0 +1,15 @@
+pkg upgrade && pkg update 
+$ pkg install imagemagick 
+$ pkg install nodejs
+$ pkg install bash 
+$ pkg install ffmpeg 
+$ termux-setup-storage 
+$ cd /sdcard 
+$ cp -r @SELF-BOT /$HOME 
+$ cd @SELF-BOT 
+$ bash install.sh 
+$ cd
+$ cd @SELF-BOT
+$ npm imagemagick
+$ npm i @adiwajshing/baileys@latest
+$ npm start
