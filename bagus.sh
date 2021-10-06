4c3ce7e239e03e978d0503b9f502205c2d577317
Server048 <64312413+Server048@users.noreply.github.com>
2021-10-06 10:56:59 +0800

    Update bagus.sh

--- bagus.sh
+++ bagus.sh
@@ -1,15 +1,21 @@
+#!/usr/bin/bash
+
+
 pkg upgrade && pkg update 
-$ pkg install imagemagick 
-$ pkg install nodejs
-$ pkg install bash 
-$ pkg install ffmpeg 
-$ termux-setup-storage 
-$ cd /sdcard 
-$ cp -r @SELF-BOT /$HOME 
-$ cd @SELF-BOT 
-$ bash install.sh 
-$ cd
-$ cd @SELF-BOT
-$ npm imagemagick
-$ npm i @adiwajshing/baileys@latest
-$ npm start
+pkg install imagemagick 
+pkg install nodejs
+pkg install bash 
+pkg install ffmpeg
+cd /sdcard 
+cp -r @SELF-BOT /$HOME 
+cd @SELF-BOT 
+bash install.sh
+clear
+echo "[*] Server By Yuki Azura"
+echo "[*] Ketik $ npm start Untuk Menjalankan"
+cd
+cd @SELF-BOT
+sleep 300
+npm imagemagick
+npm i @adiwajshing/baileys@latest
+npm start
